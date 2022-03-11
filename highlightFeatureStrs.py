2929cb8b2c739b832a5c5c76d952852fbd4acf02
Benjamin Olsen <thelarrys@gmail.com>
2022-03-11 21:25:28 +0100

    calculate most common starting notes; fix error in finding note

--- highlightFeatureStrs.py
+++ highlightFeatureStrs.py
@@ -27,17 +27,45 @@ if __name__ == "__main__":
     print(max(ngrams[n], key=ngrams[n].get))
 
     mostCommonFeatStr = max(ngrams[n], key=ngrams[n].get)
-    
+   
+    print("most common: {}".format(mostCommonFeatStr))
+
     fileOffsetDict = {}
     
     for offset, filename in startingNotes[mostCommonFeatStr]:
-        print('offset {} in {}'.format(offset, filename))
+        #print('offset {} in {}'.format(offset, filename))
         if filename not in fileOffsetDict:
+            #print("inserting {}".format(filename))
             fileOffsetDict[filename] = []
         fileOffsetDict[filename].append(offset)
+    
 
+    startingPitchHistogram = {}
+    for filepath in fileOffsetDict:
+        offsets = fileOffsetDict[filepath]
 
-    for filename in fileOffsetDict:
-        print("feature seq {} found in {} at offsets {}".format(mostCommonFeatStr, filename, fileOffsetDict[filename]))
+        #print("feature seq {} found in {} at offsets {}".format(mostCommonFeatStr, filepath, offsets))
+        
+        try:
+            s = M.converter.parse(filepath)
+        except Exception as e:
+            print("music21 couldnt parse {}".format(filepath))
+      
+        allNotes = s.flat.notes.stream()
+       
+        # what note is this
+        for offset in offsets:
+            sOut = allNotes.getElementsByOffset(offset)
+            # sOut is the first note of the ngram
+            for element in sOut:
+                p = element.pitch
+                #print('starting pitch ------> {}'.format(element.pitch))
 
+                if p not in startingPitchHistogram:
+                    startingPitchHistogram[p] = 0
+                startingPitchHistogram[p] += 1
 
+                element.style.color = 'red'
+
+    for pitch in startingPitchHistogram:
+        print("for n={}; feature {} starts on: pitch: {}; cnt {}".format(n, mostCommonFeatStr, pitch, startingPitchHistogram[pitch]))
