57d1735becfdef354aa01c174b087edeeea3fb59
Benjamin Olsen <thelarrys@gmail.com>
2022-03-12 06:19:35 +0100

    highlight correctly the notes in a feature string

--- highlightFeatureStrs.py
+++ highlightFeatureStrs.py
@@ -13,8 +13,15 @@ if __name__ == "__main__":
     # arhument is path to NGRAM JSON
     #parser = argparse.ArgumentParser()
     #parser.add_argument(
-    ngramJson = sys.argv[1]
-    startingNotesJson = sys.argv[2]
+
+    #default...
+    show = False
+    if len(sys.argv) < 3:
+        ngramJson = 'json/rast_ngrams_top500_no_redundant.json'
+        startingNotesJson = 'json/rast_startingNotes_top500_no_redundant.json'
+    else:
+        ngramJson = sys.argv[1]
+        startingNotesJson = sys.argv[2]
 
     with open(ngramJson, 'r') as f:
         ngrams = json.load(f)
@@ -23,49 +30,63 @@ if __name__ == "__main__":
         startingNotes = json.load(f)
 
     # use n = 9
-    n = str(9)
-    print(max(ngrams[n], key=ngrams[n].get))
+    for n in range(7, 15):
+        n = str(n)
+        print(max(ngrams[n], key=ngrams[n].get))
 
-    mostCommonFeatStr = max(ngrams[n], key=ngrams[n].get)
-   
-    print("most common: {}".format(mostCommonFeatStr))
-
-    fileOffsetDict = {}
-    
-    for offset, filename in startingNotes[mostCommonFeatStr]:
-        #print('offset {} in {}'.format(offset, filename))
-        if filename not in fileOffsetDict:
-            #print("inserting {}".format(filename))
-            fileOffsetDict[filename] = []
-        fileOffsetDict[filename].append(offset)
-    
-
-    startingPitchHistogram = {}
-    for filepath in fileOffsetDict:
-        offsets = fileOffsetDict[filepath]
-
-        #print("feature seq {} found in {} at offsets {}".format(mostCommonFeatStr, filepath, offsets))
-        
-        try:
-            s = M.converter.parse(filepath)
-        except Exception as e:
-            print("music21 couldnt parse {}".format(filepath))
-      
-        allNotes = s.flat.notes.stream()
+        mostCommonFeatStr = max(ngrams[n], key=ngrams[n].get)
        
-        # what note is this
-        for offset in offsets:
-            sOut = allNotes.getElementsByOffset(offset)
-            # sOut is the first note of the ngram
-            for element in sOut:
-                p = element.pitch
-                #print('starting pitch ------> {}'.format(element.pitch))
+        print("most common: {}".format(mostCommonFeatStr))
 
-                if p not in startingPitchHistogram:
-                    startingPitchHistogram[p] = 0
-                startingPitchHistogram[p] += 1
+        fileOffsetDict = {}
+        
+        for offset, filename in startingNotes[mostCommonFeatStr]:
+            #print('offset {} in {}'.format(offset, filename))
+            if filename not in fileOffsetDict:
+                #print("inserting {}".format(filename))
+                fileOffsetDict[filename] = []
+            fileOffsetDict[filename].append(offset)
+        
 
-                element.style.color = 'red'
+        startingPitchHistogram = {}
+        for filepath in fileOffsetDict:
+            featureStrOffsets = fileOffsetDict[filepath]
 
-    for pitch in startingPitchHistogram:
-        print("for n={}; feature {} starts on: pitch: {}; cnt {}".format(n, mostCommonFeatStr, pitch, startingPitchHistogram[pitch]))
+            #print("feature seq {} found in {} at offsets {}".format(mostCommonFeatStr, filepath, featureStrOffsets))
+            
+            try:
+                s = M.converter.parse(filepath)
+            except Exception as e:
+                print("music21 couldnt parse {}".format(filepath))
+          
+            allNotes = s.flat.notes.stream()
+           
+            # what note is this
+            for offset in featureStrOffsets:
+                sOut = allNotes.getElementsByOffset(offset)
+                # sOut is the first note of the ngram
+                for element in sOut:
+                    p = element.pitch
+                    print('starting pitch ------> {}'.format(element.pitch))
+
+                    if p not in startingPitchHistogram:
+                        startingPitchHistogram[p] = 0
+                    startingPitchHistogram[p] += 1
+                    
+                    noteCnt = int(n)
+                    element.style.color = 'red'
+                    nextNote = element
+                    while noteCnt > 0:
+                        nextNote = nextNote.next('Note')
+                        nextNote.style.color = 'red'
+                        noteCnt -= 1
+                    print("next: {}".format(element.next('Note')))
+            if True:
+                try:
+                    s.show()
+                    input("press enter to continue...")
+                except Exception as e:
+                    print("error showing")
+
+        for pitch in startingPitchHistogram:
+            print("for n={}; feature {} starts on: pitch: {}; cnt {}".format(n, mostCommonFeatStr, pitch, startingPitchHistogram[pitch]))
